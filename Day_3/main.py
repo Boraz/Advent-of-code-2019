e775d09e9b8d6c4750199f2e41a632d2bdf7697f
Daniel <Maxson.Daniel@gmail.com>
2019-12-17 18:04:25 -0500

    Started Day 3
    now with folders!

---
+++ Day_3/main.py
@@ -0,0 +1,21 @@
+#def calc():
+
+
+#def iterate():
+
+
+def main():
+    #input_text = open("day_3_input.txt").read().splitlines()[0]
+    #data = [int(val) for val in input_text.split(",")]
+    #print(data)
+    grid = []
+
+    for row in range(0, len(input)):
+        grid.append([])
+        for column in range(1000):
+            grid[row].append(0)
+
+
+
+if __name__ == "__main__":
+    main()
