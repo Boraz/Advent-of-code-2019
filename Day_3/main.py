846b3f8244bba64577e9b2c404556eaa0c46bcb7
Daniel <Maxson.Daniel@gmail.com>
2019-12-20 13:46:08 -0500

    Day 3
    Shamelessly copied Day 3 after seeing someone type out the code in 5 min
    Feeling salty

--- Day_3/main.py
+++ Day_3/main.py
@@ -1,21 +1,57 @@
-#def calc():
+#create a 4 item list: U D L R to hold the positions (set all to 0)
+#create a list to hold visited locations: location and visited and crossed (set first location to 0,0,0,0)
+#add the location and say we visited it
+#once we finish all location calculate the closest one
+#return the UDLR and calculate the distance
+
+#current location = []
+#visited list = []
+
+#for each item in the list
+	#input the items location (UDLR)
+		#if U then += U:
+		#elif D then += D:
+		#elif L then += L:
+		#elif R then += R:
+		#else somethign went wrong:
+	#check if the location has been visited
+		#if visited = true, then crossed = true:
+			#break
+		#else:
+			#add location to the visited list eg R75 = 0,0,0,57
+			#set visited = true
+
+#get the list of crossed locations
+#sum each location and return the min sum
 
 
-#def iterate():
+#ANNNND just watched a person do this in 5 min...
 
+A,B = open('day_3_input.txt').read().split('\n')
+A,B = [x.split(',') for x in [A,B]]
 
-def main():
-    #input_text = open("day_3_input.txt").read().splitlines()[0]
-    #data = [int(val) for val in input_text.split(",")]
-    #print(data)
-    grid = []
+DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
+DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1}
+def get_points(A):
+    x = 0
+    y = 0
+    length = 0
+    ans = {}
+    for cmd in A:
+        d = cmd[0]
+        n = int(cmd[1:])
+        assert d in ['L', 'R', 'U', 'D']
+        for _ in range(n): # I learned about the _ from this person, thanks!
+            x += DX[d]
+            y += DY[d]
+            length += 1
+            if (x,y) not in ans:
+                ans[(x,y)] = length
+    return ans
 
-    for row in range(0, len(input)):
-        grid.append([])
-        for column in range(1000):
-            grid[row].append(0)
-
-
-
-if __name__ == "__main__":
-    main()
+PA = get_points(A)
+PB = get_points(B)
+both = set(PA.keys())&set(PB.keys())
+part1 = min([abs(x)+abs(y) for (x,y) in both])
+part2 = min([PA[p]+PB[p] for p in both])
+print(part1,part2)
